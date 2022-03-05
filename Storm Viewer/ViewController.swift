f48d42f19c669daeba06ccfd1896cb3079df4a21
Sankalp-Bisht <Sankalpbisht08@gmail.com>
2022-03-05 23:04:28 +0530

    initializing

---
+++ Storm Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Sankalp Singh Bisht on 05/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
